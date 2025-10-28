68a4eda90ccce9ff994ded26971d01f64498c761
Aministrator <crucin.patricia@yahoo.com>
2025-10-28 16:56:19 +0200

    Repository - RepoDcotor

---
+++ src/main/java/com/example/hospital/repository/RepoDoctor.java
@@ -0,0 +1,66 @@
+package com.example.hospital.repository;
+
+import com.example.hospital.model.Doctor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Repository
+public class RepoDoctor {
+
+    private final List<Doctor> doctors = new ArrayList<>(); //stocheaza toti doctorii
+
+    public Doctor save(Doctor doctor) {
+        Doctor existingDoctor = findByLicenseNumber(doctor.getLicenseNumber());
+
+        if (existingDoctor != null) {
+            existingDoctor.setName(doctor.getName());
+            existingDoctor.setDepartmentID(doctor.getDepartmentID());
+            return existingDoctor;
+
+        }
+        else{
+            doctors.add(doctor);
+            return doctor;
+        }
+    }
+
+    public List<Doctor> findAllDoctors() {
+        return new ArrayList<>(this.doctors);
+    }
+
+    public Doctor findByLicenseNumber(String licenseNumber) {
+        if(licenseNumber == null) return null;
+
+        for(Doctor doctor: doctors){
+            if(doctor.getLicenseNumber().equals(licenseNumber)){
+                return doctor;
+            }
+        }
+        return null;
+    }
+
+    public boolean deleteByLicenseNumber(String licenseNumber) {
+        Doctor doctor = findByLicenseNumber(licenseNumber);
+        if (doctor != null) {
+            doctors.remove(doctor);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean existsByLicenseNumber(String licenseNumber) {
+        return findByLicenseNumber(licenseNumber) != null;
+    }
+
+    public List<Doctor> findByDepartmentID(String departmentID) {
+        List<Doctor> result = new ArrayList<>();
+        for(Doctor doctor: doctors){
+            if(doctor != null && doctor.getDepartmentID().equals(departmentID)){
+                result.add(doctor);
+            }
+        }
+        return result;
+    }
+}
